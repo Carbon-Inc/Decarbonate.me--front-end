d506a0484307b1e4342dbbe568be5fcb2c771e96
Austin Rogers <contactALRogers@gmail.com>
2017-05-15 12:41:03 -0700

    Setup LoginViewController

---
+++ Decarbonate/LoginViewController.m
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.m
+//  Decarbonate
+//
+//  Created by Kent Rogers on 5/15/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
