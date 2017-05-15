6370d62352ab061a2b0cff1dbc856eacf1a73086
Austin Rogers <contactALRogers@gmail.com>
2017-05-15 15:17:55 -0700

    early storyboard work

---
+++ Decarbonate/CarbonCalculatorViewController.m
@@ -0,0 +1,22 @@
+//
+//  CarbonCalculatorViewController.m
+//  Decarbonate
+//
+//  Created by Kent Rogers on 5/15/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import "CarbonCalculatorViewController.h"
+
+@interface CarbonCalculatorViewController ()
+
+@end
+
+@implementation CarbonCalculatorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
