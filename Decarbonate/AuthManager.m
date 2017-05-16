7f5fd7e00c77006c38d74a884dd435d5c9d4f071
Adrian Kenepah-Martin <adriankenepah-martin@Adrians-MacBook-Pro.local>
2017-05-15 20:28:34 -0700

    Installed CocoaPods.

---
+++ Decarbonate/AuthManager.m
@@ -0,0 +1,14 @@
+//
+//  AuthManager.m
+//  Decarbonate
+//
+//  Created by Adrian Kenepah-Martin on 5/15/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import "AuthManager.h"
+#import "Credentials.h"
+
+@implementation AuthManager
+
+@end
