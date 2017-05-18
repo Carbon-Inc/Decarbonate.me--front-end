a8eca38542f0cb7580495f3eeade9ca666282432
Austin Rogers <contactALRogers@gmail.com>
2017-05-18 12:43:33 -0700

    Am getting coords from an address string

--- Decarbonate/CarbonCalcViewController.m
+++ Decarbonate/CarbonCalcViewController.m
@@ -8,6 +8,8 @@
 
 #import "CarbonCalcViewController.h"
 
+@import MapKit;
+
 @interface CarbonCalcViewController ()
 
 @end
@@ -17,6 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self getLocationFromAddressString:@"2409 141st pl SE, Mill Creek, WA"];
 }
 
 - (IBAction)calculateButtonPressed:(id)sender {
@@ -25,5 +28,29 @@
 - (IBAction)payButtonPressed:(id)sender {
 }
 
+
+
+-(CLLocationCoordinate2D) getLocationFromAddressString: (NSString*) addressStr {
+    double latitude = 0, longitude = 0;
+    NSString *esc_addr =  [addressStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *req = [NSString stringWithFormat:@"https://maps.google.com/maps/api/geocode/json?sensor=false&address=%@", esc_addr];
+    NSString *result = [NSString stringWithContentsOfURL:[NSURL URLWithString:req] encoding:NSUTF8StringEncoding error:NULL];
+    if (result) {
+        NSScanner *scanner = [NSScanner scannerWithString:result];
+        if ([scanner scanUpToString:@"\"lat\" :" intoString:nil] && [scanner scanString:@"\"lat\" :" intoString:nil]) {
+            [scanner scanDouble:&latitude];
+            if ([scanner scanUpToString:@"\"lng\" :" intoString:nil] && [scanner scanString:@"\"lng\" :" intoString:nil]) {
+                [scanner scanDouble:&longitude];
+            }
+        }
+    }
+    CLLocationCoordinate2D center;
+    center.latitude=latitude;
+    center.longitude=longitude;
+    NSLog(@"View Controller get Location Logitute : %f",center.latitude);
+    NSLog(@"View Controller get Location Latitute : %f",center.longitude);
+    return center;
+    
+}
 
 @end
