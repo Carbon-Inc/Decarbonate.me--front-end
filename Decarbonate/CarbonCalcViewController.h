533ff86f2f081e57f731806b08c91638880a554e
Austin Rogers <contactALRogers@gmail.com>
2017-05-18 10:23:39 -0700

    Added boilerplate code +buttons/labels. Linked everything up.

---
+++ Decarbonate/CarbonCalcViewController.h
@@ -0,0 +1,21 @@
+//
+//  CarbonCalcViewController.h
+//  Decarbonate
+//
+//  Created by Kent Rogers on 5/18/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarbonCalcViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *transportTypeSegment;
+
+@property (weak, nonatomic) IBOutlet UILabel *startPointLabel;
+@property (weak, nonatomic) IBOutlet UILabel *endPointLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
+@property (weak, nonatomic) IBOutlet UIButton *payButton;
+
+@end
---
+++ Decarbonate/CarbonCalcViewController.m
@@ -0,0 +1,29 @@
+//
+//  CarbonCalcViewController.m
+//  Decarbonate
+//
+//  Created by Kent Rogers on 5/18/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import "CarbonCalcViewController.h"
+
+@interface CarbonCalcViewController ()
+
+@end
+
+@implementation CarbonCalcViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)calculateButtonPressed:(id)sender {
+}
+
+- (IBAction)payButtonPressed:(id)sender {
+}
+
+
+@end
